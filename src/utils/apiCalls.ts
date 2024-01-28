4efad202e6536d436b91af69a75840bd89bf787a
decocereus <psyking07@gmail.com>
2024-01-29 01:04:00 +0530

    debugging chrome.runtime.sendMessage

--- src/utils/apiCalls.ts
+++ src/utils/apiCalls.ts
@@ -33,7 +33,8 @@ export const googleAuthController = async (authenticationCode: string) => {
     const response = await axios.get(
       `https://visited-server-backend.onrender.com/api/v1/auth/google/callback?code=${authenticationCode}`
     );
-    chrome.runtime.sendMessage({ type: "authUser", data: response.data });
+    console.log(chrome.runtime);
+    //chrome.runtime.sendMessage({ type: "authUser", data: response.data });
     return response.data;
   } catch (error) {
     console.error("Error exchanging code for token:", error);
