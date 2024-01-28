34e191ff0c35433864123e86cc377948e5c9136a
decocereus <psyking07@gmail.com>
2024-01-28 23:55:30 +0530

    Removed content script

--- src/components/GoogleAuthHandler.tsx
+++ src/components/GoogleAuthHandler.tsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { loggedInUser } from "../features/authSlice";
 import { googleAuthController } from "../utils/apiCalls";
-import { runtime } from "webextension-polyfill";
 
 const GoogleAuthHandler = () => {
   const navigate = useNavigate();
@@ -11,28 +10,12 @@ const GoogleAuthHandler = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const authenticationCode = urlParams.get("code");
 
-  const sendGoogleIdToExtension = (googleId: string) => {
-    const message = { type: "authUser", googleId: googleId };
-
-    runtime.sendMessage(message).then(
-      (response: any) => {
-        // Handle the response from the content script
-        console.log(response);
-      },
-      (error: any) => {
-        // Handle the error if needed
-        console.error(error);
-      }
-    );
-  };
-
   const handleGoogleAuthFlow = async () => {
     if (authenticationCode) {
       let response = await googleAuthController(authenticationCode);
       console.log(response);
       dispatch(loggedInUser(response));
       const googleId = response.googleId;
-      sendGoogleIdToExtension(googleId);
 
       navigate("/dashboard");
     }
